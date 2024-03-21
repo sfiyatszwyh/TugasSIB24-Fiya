c70000e2f09a5a5b36e94b924f758c9c6e1c5795
sfiyatszwyh <sfiyatszwyh.co.id>
2024-03-21 22:48:27 +0700

    Tugas P8

--- js/api.js
+++ js/api.js
@@ -1,4 +1,4 @@
-fetch('https://crudcrud.com/api/10edc5cfcde149539635f4b12cd3012d/produk', { mode: 'no-cors' })
+fetch('https://crudcrud.com/api/10edc5cfcde149539635f4b12cd3012d/produk')
     .then(response => response.json())
     .then(data => {
         const productTable = document.getElementById('productTable');
@@ -6,14 +6,11 @@ fetch('https://crudcrud.com/api/10edc5cfcde149539635f4b12cd3012d/produk', { mode
         data.forEach(product => {
             const row = document.createElement('tr');
             row.innerHTML = `
-                <td>${product._id}</td>
-                <td>${product.nama_produk}</td>
-                <td>${product.jumlah}</td>
-                <td>${product.harga}</td>
-            `;
+        <td>${product._id}</td>
+        <td>${product.nama_produk}</td>
+        <td>${product.jumlah}</td>
+        <td>${product.harga}</td>
+      `;
             productTable.querySelector('tbody').appendChild(row);
         });
     })
-    .catch(error => {
-        console.error('Error fetching data:', error);
-    });
